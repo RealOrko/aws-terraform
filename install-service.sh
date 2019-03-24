308b5d7e44362fbec5eb1b4449c9392b862b5c43
RealOrko <realorko@nowhere.com>
2019-03-24 18:01:38 +0000

    Changing the kestrel port to 80

--- install-service.sh
+++ install-service.sh
@@ -8,7 +8,6 @@ sudo apt-get update -y
 sudo apt-get install -y dotnet-sdk-2.2
 
 sudo mkdir /opt/dotnet-service
-sudo cd /opt/dotnet-service
 sudo dotnet new mvc
 sudo dotnet build
 sudo dotnet publish -c Release -r linux-x64 -o /opt/dotnet-service/
@@ -16,5 +15,5 @@ sudo dotnet publish -c Release -r linux-x64 -o /opt/dotnet-service/
 sudo cp -f ~/dotnet-service.service /etc/systemd/system/
 sudo systemctl daemon-reload  
 sudo systemctl enable dotnet-service.service  
-sudo systemctl start dotnet-service.service  
-sudo systemctl status dotnet-service.service  
+#sudo systemctl start dotnet-service.service  
+#sudo systemctl status dotnet-service.service  
